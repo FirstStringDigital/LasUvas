8eaf2d56de88b0c1cedfa943d06275e52beb0a0a
Natalie Cervantes <natalie.cervantes@gmail.com>
2019-08-12 13:49:38 -0600

    ADDED WORDPRESS PLUGIN Added the wordpress plugin to gatsby to obtain wordpress content / nodes to gatsby site

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,63 @@
 module.exports = {
   siteMetadata: {
     title: `Las Uvas`,
-    description: `Place Site Description HERE`,
+    description: `Specializing In Mexican Wine Tastings`,
     author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'GatsbyJS',
+        short_name: 'Gatsby',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/las_uvas_logo.png',
+      },  // gatsby-plugin-manifest enables the web app manifest required for PWA. Will need to update with a proper icon image and colors for las uvas
+    },
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        baseUrl: "localhost:8888/lasuvas_cms",
+        protocol: "http",
+        hostingWPCOM: false,
+        useACF: true,
+        acfOptionPageIds: [],
+        verboseOutput: false,
+        perPage: 100,
+        searchAndReplaceContentUrls: {
+          sourceUrl: "http://www.lasuvasmexico.com",
+          replacementUrl: "http://localhost:8888/lasuvas_cms/",
+        },
+        concurrentRequests: 10,
+        includedRoutes: [
+          "**/categories",
+          "**/posts",
+          "**/pages",
+          "**/media",
+          "**/tags",
+          "**/taxonomies",
+          "**/users",
+        ],
+        excludedRoutes: [],
+        normalizer: function({ entities}) {
+          return entities
+        },
+      },
+    },
+    'gatsby-plugin-sitemap',
+    // this plugin automatically generates a sitemap for your site. 
+    
+    'gatsby-plugin-offline',
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    //To learn more, visit https://gatsby.dev/offline
+  
     `gatsby-plugin-transition-link`,
     {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
