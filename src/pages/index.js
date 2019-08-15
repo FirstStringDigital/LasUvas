7d46e0a650c899085564df16f8e70a8616f0759e
Natalie Cervantes <natalie.cervantes@gmail.com>
2019-08-15 12:45:35 -0600

    HOOK UP LAS UVAS HOME PAGE CONTENT FROM WORDPRESS Added functionality to the index.js homepage to pull content from Wordpress content / post type the Las Uvas Homepage.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from '../components/layout'
 import Head from '../components/head'
 import IndexStyle from'./index.module.scss'
@@ -7,17 +8,17 @@ import AniLink from "gatsby-plugin-transition-link/AniLink"
 import MainLogo from '../images/las_uvas_logo.png'
 import MainBkgd from '../images/main-background.png'
 
-const IndexPage = () => {
+const IndexPage = ({data}) => {
     return (
         <Layout>
             <Head title="Home" />
             <div className="row">
                 <Col lg={6} md={12} className={IndexStyle.mainSection}>
                     <img src={MainLogo} alt="Las Uvas Logo" className={IndexStyle.mainLogo} />
-                    <p className={IndexStyle.mainText}>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fermentum ut dolor a laoreet. Fusce imperdiet, dolor eu fermentum rhoncus, ligula nibh dapibus sem, in rutrum tortor turpis eget tortor. </p>
+                    <p className={IndexStyle.mainText}>{data.wordpressWpHome.acf.content}</p>
                     <div className={IndexStyle.mobileBtn}>
-                        <AniLink swipe direction="right" entryOffset={100} to="/mission" className={IndexStyle.btn}>OUR MISSION</AniLink>
-                        <AniLink swipe direction="left" entryOffset={100} to="/tastings" className={IndexStyle.btn}>DISCOVER OUR TASTINGS</AniLink>
+                        <AniLink swipe direction="right" entryOffset={100} to="/mission" className={IndexStyle.btn}>{data.wordpressWpHome.acf.subtitle2}</AniLink>
+                        <AniLink swipe direction="left" entryOffset={100} to="/tastings" className={IndexStyle.btn}>{data.wordpressWpHome.acf.subtitle}</AniLink>
                     </div>
                     <img src={MainBkgd} alt="Glasses of Wine" className={IndexStyle.mainBkgd} />
                 </Col>
@@ -26,14 +27,14 @@ const IndexPage = () => {
                         <Col md={4} className={IndexStyle.rightTopImg}></Col>
                         <Col md={8} className={IndexStyle.rightTopText}>
                             <h1 className="text-center">Our Tastings</h1>
-                            <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fermentum ut dolor a laoreet. Fusce imperdiet, dolor eu fermentum rhoncus, ligula nibh dapibus sem, in rutrum tortor turpis eget tortor. </p>
+                            <p>{data.wordpressWpHome.acf.subcontent}</p>
                             <AniLink swipe direction="up" entryOffset={100} to="/tastings" className={IndexStyle.btn}>DISCOVER OUR TASTINGS</AniLink>
                         </Col>
                     </div>
                     <div className={IndexStyle.rightBottomRow}>
                         <Col md={12} className={IndexStyle.rightBottomSection}>
                             <h1 className="text-center">Our Mission</h1>
-                            <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fermentum ut dolor a laoreet. Fusce imperdiet, dolor eu fermentum rhoncus, ligula nibh dapibus sem, in rutrum tortor turpis eget tortor. </p>
+                            <p>{data.wordpressWpHome.acf.subcontent2}</p>
                             <AniLink swipe direction="up" entryOffset={100} to="/mission" className={IndexStyle.btn}>LEARN MORE</AniLink>
                         </Col>
                     </div>
@@ -44,3 +45,19 @@ const IndexPage = () => {
 }
 
 export default IndexPage
+
+
+export const query = graphql`
+query HomePage {
+    wordpressWpHome {
+        content
+        acf {
+            content
+            subtitle
+            subcontent
+            subtitle2
+            subcontent2
+        }
+    }
+}
+`
