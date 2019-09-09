d223ee0e1ae67596615b3ff081531e92e2fb00db
stevekim72 <steve.kim72@yahoo.com>
2019-09-09 11:10:20 -0600

    Created: vlog-component.js; Edited: vlog.js; Moved: vlog.module.scss

--- src/pages/vlog.js
+++ src/pages/vlog.js
@@ -2,11 +2,12 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from '../components/layout'
 import Head from '../components/head'
-import VlogStyle from'./vlog.module.scss'
+import Vlog from "../components/vlog-component"
+import OurTastings from "../components/our-tastings"
+import OurMission from "../components/our-mission"
+import VlogStyle from'../styles/vlog.module.scss'
 import { Col } from 'react-bootstrap'
-import AniLink from "gatsby-plugin-transition-link/AniLink"
 import MainBkgd from '../images/main-background.png'
-import Video from '../components/video'
 
 const VlogPage = ({data}) => {
     return (
@@ -17,29 +18,20 @@ const VlogPage = ({data}) => {
                     <h1 className={VlogStyle.mainTitleBold}>Vlog</h1>
                     <h1 className={VlogStyle.mainSectionTitle}>Friday Night With The Kims</h1>
                     {/*Video Content here */}
-                    <div className={VlogStyle.videoSection}>{data.allWordpressPost.edges.map(document => (
-                        <li key={document.node.id}>
-                            <Video videoSrcURL={document.node.acf.youtubeurl} videoTitle="Featured Video"/>
-                        </li>
-                    ))}
-                        {/* End Video Content */}
-                    </div>
+                    <Vlog />
+                    {/* End Video Content */}
                     <img src={MainBkgd} alt="Glasses of Wine" className={VlogStyle.mainBkgd} />
                 </Col>
                 <Col lg={6} md={0} className={VlogStyle.rightSection}>
                     <div className="row">
                         <Col md={4} className={VlogStyle.rightTopImg}></Col>
                         <Col md={8} className={VlogStyle.rightTopText}>
-                            <h1 className="text-center">Our Tastings</h1>
-                            <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fermentum ut dolor a laoreet. Fusce imperdiet, dolor eu fermentum rhoncus, ligula nibh dapibus sem, in rutrum tortor turpis eget tortor. </p>
-                            <AniLink swipe direction="up" entryOffset={100} to="/tastings" className={VlogStyle.btn}>DISCOVER OUR TASTINGS</AniLink>
+                            <OurTastings />
                         </Col>
                     </div>
                     <div className={VlogStyle.rightBottomRow}>
                         <Col md={12} className={VlogStyle.rightBottomSection}>
-                            <h1 className="text-center">Our Mission</h1>
-                            <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fermentum ut dolor a laoreet. Fusce imperdiet, dolor eu fermentum rhoncus, ligula nibh dapibus sem, in rutrum tortor turpis eget tortor. </p>
-                            <AniLink swipe direction="up" entryOffset={100} to="/mission" className={VlogStyle.btn}>LEARN MORE</AniLink>
+                            <OurMission />
                         </Col>
                     </div>
                 </Col>
