9a5f1e4056960698a1bbb4d98846a4e5045b8d73
stevekim72 <steve.kim72@yahoo.com>
2019-10-07 13:32:21 -0600

    add Google Analytics plugin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -131,7 +131,25 @@ module.exports = {
           },
         ],
       },
-    }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-119405948-8",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "auto",
+      },
+    },
   ]
 }
     
