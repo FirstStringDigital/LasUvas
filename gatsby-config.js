15e1a127bd559c6334b2a83c198583e9e632bf9d
stevekim72 <steve.kim72@yahoo.com>
2019-10-08 15:39:11 -0600

    created inital 404 page.

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,39 +9,39 @@ module.exports = {
     title: `Las Uvas`,
     description: `Specializing In Mexican Wine Tastings`,
     author: `@gatsbyjs`,
-    siteUrl: 'https://lasuvasmexico.com'
+    siteUrl: "https://lasuvasmexico.com",
   },
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     //'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    "gatsby-plugin-sharp",
     `gatsby-plugin-sitemap`,
     {
-      resolve: 'gatsby-plugin-robots-txt',
+      resolve: "gatsby-plugin-robots-txt",
       options: {
-        host: 'https://lasuvasmexico.com',
-        sitemap: 'https://lasuvasmexico.com',
-        policy: [{ userAgent: '*', allow: '/' }]
-      }
+        host: "https://lasuvasmexico.com",
+        sitemap: "https://lasuvasmexico.com",
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
     },
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: 'Las Uvas',
-        short_name: 'Las Uvas',
-        start_url: '/',
-        background_color: '#900561',
-        theme_color: '#900561',
-        display: 'standalone',
-        icon: 'src/images/las_uvas_logo.png',
-        orientation: 'portrait',
-      },  // gatsby-plugin-manifest enables the web app manifest required for PWA. Will need to update with a proper icon image and colors for las uvas
+        name: "Las Uvas",
+        short_name: "Las Uvas",
+        start_url: "/",
+        background_color: "#900561",
+        theme_color: "#900561",
+        display: "standalone",
+        icon: "src/images/las_uvas_logo.png",
+        orientation: "portrait",
+      }, // gatsby-plugin-manifest enables the web app manifest required for PWA. Will need to update with a proper icon image and colors for las uvas
     },
     {
       resolve: "gatsby-source-wordpress",
       options: {
-        baseUrl: "lasuvasadmin.com", //"localhost:8888/lasuvas_cms", 
+        baseUrl: "lasuvasadmin.com", //"localhost:8888/lasuvas_cms",
         protocol: "http",
         hostingWPCOM: false,
         useACF: true,
@@ -53,8 +53,8 @@ module.exports = {
         verboseOutput: false,
         perPage: 100,
         searchAndReplaceContentUrls: {
-          sourceUrl: "lasuvasadmin.com", //"localhost:8888/lasuvas_cms", 
-          replacementUrl: "lasuvasadmin.com", //"localhost:8888/lasuvas_cms", 
+          sourceUrl: "lasuvasadmin.com", //"localhost:8888/lasuvas_cms",
+          replacementUrl: "lasuvasadmin.com", //"localhost:8888/lasuvas_cms",
         },
         concurrentRequests: 10,
         includedRoutes: [
@@ -72,22 +72,22 @@ module.exports = {
           "**/testimonials",
         ],
         excludedRoutes: [],
-        normalizer: function({ entities}) {
+        normalizer: function({ entities }) {
           return entities
         },
       },
     },
-    'gatsby-plugin-sitemap',
-    // this plugin automatically generates a sitemap for your site. 
-    
-    // this (optional) plugin enables Progressive Web App + Offline functionality. The plugin creates a service worker for us by leveraging Google’s Workbox 
+    "gatsby-plugin-sitemap",
+    // this plugin automatically generates a sitemap for your site.
+
+    // this (optional) plugin enables Progressive Web App + Offline functionality. The plugin creates a service worker for us by leveraging Google’s Workbox
     // library. We get automatic file caching, enabling pages to be available offline after the user’s first visit.
     //To learn more, visit https://gatsby.dev/offline
     {
-      resolve: 'gatsby-plugin-offline',
-    options: {
-      precachePages: ['/tastings/', '/mission/'],
-      /* workboxConfig: {
+      resolve: "gatsby-plugin-offline",
+      options: {
+        precachePages: ["/tastings/", "/mission/"],
+        /* workboxConfig: {
         importWorkboxFrom: `cdn`,
         runtimeCaching: [{ 
           urlPattern: 'lasuvasmexico/.*',
@@ -117,9 +117,8 @@ module.exports = {
         },
       }],
     }, */
-  },
-},
-  
+      },
+    },
     `gatsby-plugin-transition-link`,
     {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
@@ -140,6 +139,5 @@ module.exports = {
         head: true,
       },
     },
-  ]
+  ],
 }
-    
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,40 @@
 import React from "react"
-import Layout from "../components/layout"
-//import SEO from "../components/seo" - removed as I ddin't seem need. If we think it is I grabbed this code from ELC blog file which was based 
+import { Col } from 'react-bootstrap'
+import { Link } from "gatsby"
+import Layout from '../components/layout'
+import FourOFourMain from "../components/404-main"
+import "../styles/global.css"
+import MainBkgd from '../images/main-background.png'
+//import SEO from "../components/seo" - removed as I ddin't seem need. If we think it is I grabbed this code from ELC blog file which was based
 // off the Gatsby Starter Default template
 
-const NotFoundPage = () => (
-  <Layout>
-    <h1>404: NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  return (
+    <Layout>
+      <div className="row">
+        <Col lg={6} md={12} className="main-section">
+          <FourOFourMain />
+          <img src={MainBkgd} alt="Glasses of Wine" className="main-bkgd" />
+        </Col>
+        <Col lg={6} md={0} className="right-section">
+          <div className="row">
+            <Col md={4} className="right-top-img"></Col>
+            <Col md={8} className="right-top-text">
+              <h1 className="text-center">Our Pages</h1>
+              <Link to="/tastings" className="btn">TASTINGS PAGE</Link><br />
+              <Link to="/mission" className="btn">MISSION/FAQ PAGE</Link><br />
+              <Link to="/vlog" className="btn">VLOG</Link><br />
+              <Link to="/testimonials" className="btn">TESTIMONIALS PAGE</Link>
+            </Col>
+          </div>
+          <div className="right-bottom-row">
+            <Col md={12} className="right-bottom-section">
+            </Col>
+          </div>
+        </Col>
+      </div>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
