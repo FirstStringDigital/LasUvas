be274c395d82150118131775f6e9448672927676
stevekim72 <steve.kim72@yahoo.com>
2019-10-11 12:37:07 -0600

    Added Online/Offline indicator plugin for tasting page.

--- src/components/chefs-dinner.js
+++ src/components/chefs-dinner.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
+import { Offline, Online } from "react-detect-offline"
 import "../styles/global.css"
 
 
@@ -21,12 +22,14 @@ export default () => (
             <h1 className="main-section-title">{data.wordpressWpTasting.acf.subtitle3}</h1>
             <p className="main-text">{data.wordpressWpTasting.acf.subcontent3}</p>
             <p className="price">{data.wordpressWpTasting.acf.price3}</p>
+            <Online>
             {/*Calendly*/}
             <link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet"/>
             <script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript"></script>
             <a className="btn" href="https://calendly.com/lasuvas/chefs-dinner?text_color=000000&primary_color=900561"  onclick="Calendly.initPopupWidget({url: 'https://calendly.com/lasuvas/chefs-dinner'});return false;">BOOK CHEF'S DINNER</a>
-
             {/*Calendly*/}
+            </Online>
+            <Offline><p className="offline-text">You must be online to book an event.</p></Offline>
             </div>
         )}
         />
