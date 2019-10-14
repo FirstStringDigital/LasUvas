6d0d477176b2dca6996ccea1bdabe0e23c2e5a72
stevekim72 <steve.kim72@yahoo.com>
2019-10-14 12:15:59 -0600

    Rearranged the tasting page on Dektop view.

--- src/components/chefs-dinner-desktop.js
+++ src/components/chefs-dinner-desktop.js
@@ -7,27 +7,27 @@ import TastingStyle from "../styles/tasting.module.scss"
 
 export default () => (
     <StaticQuery query={graphql`
-        query BasicTastingMobile {
+        query ChefsDinnerDesktop {
             wordpressWpTasting {
                 acf{
-                    subtitle
-                    subcontent
-                    price1
+                    subtitle3
+                    subcontent3
+                    price3
                 }
             }
         }
         `}
         render={data => (
-            <div className={TastingStyle.basicTastingMobile}>
+            <div className={TastingStyle.chefsDinnerDesktop}>
             <h1 className="main-title-bold">Discover Our Tastings</h1>
-            <h1 className="main-section-title">{data.wordpressWpTasting.acf.subtitle}</h1>
-            <p className="main-text">{data.wordpressWpTasting.acf.subcontent}</p>
-            <p className="price">{data.wordpressWpTasting.acf.price1}</p>
+            <h1 className="main-section-title">{data.wordpressWpTasting.acf.subtitle3}</h1>
+            <p className="main-text">{data.wordpressWpTasting.acf.subcontent3}</p>
+            <p className="price">{data.wordpressWpTasting.acf.price3}</p>
             <Online>
             {/*Calendly*/}
             <link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet"/>
             <script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript"></script>
-            <a className="btn" href="https://calendly.com/lasuvas/basic-tasting?text_color=000000&primary_color=900561"  onclick="Calendly.initPopupWidget({url: 'https://calendly.com/lasuvas/basic-tasting'});return false;">BOOK BASIC TASTING</a>
+            <a className="btn" href="https://calendly.com/lasuvas/chefs-dinner?text_color=000000&primary_color=900561"  onclick="Calendly.initPopupWidget({url: 'https://calendly.com/lasuvas/chefs-dinner'});return false;">BOOK CHEF'S DINNER</a>
             {/*Calendly*/}
             </Online>
             <Offline><p className="offline-text-tasting">You must be online to book an event.</p></Offline>
