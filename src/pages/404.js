ea54f8494722393682c07cd1955fa3cf46a055a8
stevekim72 <steve.kim72@yahoo.com>
2019-10-14 13:09:32 -0600

    completed 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,11 @@
 import React from "react"
-import { Col } from 'react-bootstrap'
-import { Link } from "gatsby"
-import Layout from '../components/layout'
+import { Col } from "react-bootstrap"
+import Layout from "../components/layout"
 import FourOFourMain from "../components/404-main"
+import OurTastings from "../components/our-tastings"
+import OurMission from "../components/our-mission"
 import "../styles/global.css"
-import MainBkgd from '../images/main-background.png'
+import MainBkgd from "../images/main-background.png"
 //import SEO from "../components/seo" - removed as I ddin't seem need. If we think it is I grabbed this code from ELC blog file which was based
 // off the Gatsby Starter Default template
 
@@ -20,15 +21,12 @@ const NotFoundPage = () => {
           <div className="row">
             <Col md={4} className="right-top-img"></Col>
             <Col md={8} className="right-top-text">
-              <h1 className="text-center">Our Pages</h1>
-              <Link to="/tastings" className="btn">TASTINGS PAGE</Link><br />
-              <Link to="/mission" className="btn">MISSION/FAQ PAGE</Link><br />
-              <Link to="/vlog" className="btn">VLOG</Link><br />
-              <Link to="/testimonials" className="btn">TESTIMONIALS PAGE</Link>
+              <OurMission />
             </Col>
           </div>
           <div className="right-bottom-row">
             <Col md={12} className="right-bottom-section">
+              <OurTastings />
             </Col>
           </div>
         </Col>
