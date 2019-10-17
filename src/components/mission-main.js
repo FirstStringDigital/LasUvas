51de1db96ad9e5916e00e71ed995e4982a69f00e
Natalie Cervantes <natalie.cervantes@gmail.com>
2019-10-17 15:09:36 -0600

    Updated text on missin-main.js to make more sense on mobile.

--- src/components/mission-main.js
+++ src/components/mission-main.js
@@ -18,7 +18,7 @@ export default () => (
                 <h1 className="main-title-bold">Our Mission</h1>
                 <h1 className="main-section-title">{data.wordpressWpMission.acf.aboutUsTitle}</h1>
                 <p className="main-text">{data.wordpressWpMission.acf.aboutUsContent}</p>
-                <p className="main-text">Swipe for Frequently Asked Questions and the nitty-gritty.</p>
+                <p className="main-text">Swipe for more details, Frequently Asked Questions and the nitty-gritty.</p>
             </div>
         )}
         />
